bb851bd556669d62ff9c933cf82487cc0cb18aad
juddB <Juddbahoui@gmail.com>
2021-02-25 14:03:48 -0500

    commit tree ab61f52356eb9b87b95774bc7d9be3feeda38b6d

---
+++ lib/players/human.rb
@@ -0,0 +1,17 @@
+module Players
+    class Human < Player 
+
+        def move(board)
+
+        puts "Please enter your selection"
+            gets.strip
+        end
+
+
+
+
+
+
+    end
+    
+end
